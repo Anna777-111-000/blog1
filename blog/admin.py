6c6aafa6f3a08b6b52836a37d031fd37c19d7739
Anna777-111-000 <anneremneva@yandex.ru>
2025-02-13 19:17:22 +0300

    Update admin.py

--- blog/admin.py
+++ blog/admin.py
@@ -1,14 +1,30 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Category, Post
+from .models import Author, Category, Post, Tag, Comment
 
 
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
-    ...
+    list_display = ('title', 'slug') 
+    search_fields = ('title', 'description') 
+    prepopulated_fields = {'slug': ('title',)} 
 
 
 @admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
-    ...
+    list_display = ('title', 'author', 'category', 'publication_date', 'is_published')
+    list_filter = ('author', 'category', 'publication_date', 'is_published')
+    search_fields = ('title', 'content')
+    prepopulated_fields = {'slug': ('title',)}
+    date_hierarchy = 'publication_date'
+    ordering = ('-publication_date',)  
+    fieldsets = ( 
+        ('Основная информация', {
+            'fields': ('title', 'slug', 'category', 'content')
+        }),
+        ('Дополнительная информация', {
+            'fields': ('picture', 'publication_date', 'is_published'),
+            'classes': ('collapse',) 
+        }),
+    )
